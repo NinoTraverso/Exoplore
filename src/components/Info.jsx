506670d53d981c4545eb9bf28e3fc1ccbe77b1d3
NinoTraverso <njt98git@proton.me>
2024-02-24 17:24:04 +0000

    fixed info page layout

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -9,40 +9,42 @@ function Info() {
 
       <h1 className="text-center my-5">About Exoplore</h1>
       <div className="mx-3 my-5">
-        <h3>Description:</h3>
-        <h5>
+        <h3 className="text-center mt-5">Description:</h3>
+        <h5 className="text-center mb-5">
           "Exoplore" is a React-based web application designed to showcase data
           on exoplanets sourced from NASA's API. With sleek and intuitive
           design, this project offers users a comprehensive look into the vast
           expanse of exoplanetary discoveries.
         </h5>
-        <h3>Features:</h3>
+        <h3 className="text-center mt-5">Features:</h3>
         <ul>
           <li>
             NASA API Integration: Leveraging NASA's API, our project aggregates
             a wealth of data on exoplanets, including their names, discovery
             methods, and orbital characteristics.
           </li>
-          <li>
+          <li className="mb-5">
             Interactive Tables: Through user-friendly tables, users can easily
             navigate and explore the extensive database of exoplanets. Each
             entry provides detailed information, allowing enthusiasts to dive
             deep into the world of exoplanetary science.
           </li>
         </ul>
-        <h3>Objective:</h3>
-        <h5>
+        <h3 className="text-center mt-5">Objective:</h3>
+        <h5 className="text-center mb-5">
           The primary goal of "Exoplore" is to provide a seamless and engaging
           platform for users to learn about exoplanets. Whether you're a space
           enthusiast, student, or researcher, this project offers a captivating
           glimpse into the wonders of the cosmos beyond our solar system.
         </h5>
       </div>
-      <div>
-        <h3>You can find out more about this project on Github. </h3>
-        <div className="text-decoration-none m-3">
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <h3 className="text-center">
+          You can find out more about this project on Github.{" "}
+        </h3>
+        <div className="text-decoration-none m-4">
           <a
-            href="www.google.com"
+            href="https://www.google.com/"
             className="repoLink text-decoration-none text-white"
           >
             <i class="devicon-github-original-wordmark"></i>
