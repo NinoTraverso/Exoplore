fcb4954cf0dbedd3a57f262d1a9519b6c88a5a59
NinoTraverso <njt98git@proton.me>
2024-03-18 09:45:40 +0000

    started to implement the data I want to include in the exoplanet's data list

--- src/components/Exoplanets.tsx
+++ src/components/Exoplanets.tsx
@@ -16,8 +16,21 @@ const BASE_URL =
 interface Table {
   kepid: number;
   koi_period: number;
+  ra: number; //Right Ascension (deg)
+  dec: number; //Declination (deg)
+  koi_kepmag: number; //Kepler-band (mag)
+  koi_gmag: number; //g-band mag
   koi_duration: number;
   koi_prad: number;
+  koi_eccen: number;
+  koi_ror: number; //planet/star radius ratio
+  koi_sma: number; //orbit semi-major axis (AU)
+  koi_count: number; //number of planets
+  koi_steff: number; //star temperature (K)
+  koi_smet: number; //stellar metallicity (The base-10 logarithm of the Fe to H ratio at the surface of the star, normalized by the solar Fe to H ratio)
+  koi_srad: number; //Star radius in Solar Radii
+  koi_smass: number; //Mass of the star in solar masses
+  koi_sage: number; //stellar age in Gigayears
 }
 
 function GettingExoTables() {
@@ -43,6 +56,9 @@ function GettingExoTables() {
           throw new Error("Failed to fetch data");
         }
         const data = await response.json();
+
+        console.log("API Response:", data);
+
         setTables(data);
       } catch (error) {
         if (error.name === "AbortError") {
@@ -87,12 +103,46 @@ function GettingExoTables() {
               <ul>
                 {table.map((entry) => (
                   <li key={entry.kepid}>
-                    <span>ID: </span>
-                    {entry.kepid} | <span>Orbital Period: </span>
-                    {entry.koi_period} <span> days</span> !{" "}
-                    <span>Transidion duration:</span> {entry.koi_duration}{" "}
-                    <span> hours</span> | <span>Planetary Radius: </span>{" "}
-                    {entry.koi_prad} <span>Earth Radii </span>
+                    <span className="dataName">ID: </span>
+                    {entry.kepid}| <span className="dataName">RA: </span>{" "}
+                    {entry.ra} <span> deg </span>|{" "}
+                    <span className="dataName">Dec: </span> {entry.dec}{" "}
+                    <span> deg </span>|{" "}
+                    <span className="dataName">Kepler-band: </span>{" "}
+                    {entry.koi_kepmag} <span> mag </span>|{" "}
+                    <span className="dataName">g-band: </span> {entry.koi_gmag}{" "}
+                    <span> mag </span>|{" "}
+                    <span className="dataName">Orbital Period: </span>{" "}
+                    {entry.koi_period} <span> days </span>|{" "}
+                    <span className="dataName">Transition duration:</span>{" "}
+                    {entry.koi_duration} <span> hours</span>|{" "}
+                    <span className="dataName">Planetary Radius: </span>{" "}
+                    {entry.koi_prad} <span> Earth Radii </span>|{" "}
+                    <span className="dataName">Orbit Semi-Major Axis: </span>{" "}
+                    {entry.koi_sma} <span> AU </span>|{" "}
+                    <span className="dataName">Eccentricity: </span>{" "}
+                    {entry.koi_eccen} <span> </span>|{" "}
+                    <span className="dataName">: </span> {entry.koi_prad}{" "}
+                    <span> </span>|{" "}
+                    <span className="dataName">Planets number: </span>{" "}
+                    {entry.koi_count} <span> </span>|{" "}
+                    <span className="dataName">Planet/Star Radius Ratio: </span>{" "}
+                    {entry.koi_ror} <span> </span>|{" "}
+                    <span className="dataName">Star Temperature: </span>{" "}
+                    {entry.koi_steff} <span> Kelvin </span>|{" "}
+                    <span className="dataName">Stellar Metallicity: </span>{" "}
+                    {entry.koi_smet}{" "}
+                    <span>
+                      {" "}
+                      The base-10 logarithm of the Fe to H ratio at the surface
+                      of the star, normalized by the solar Fe to H ratio{" "}
+                    </span>
+                    | <span className="dataName">Star Radius: </span>{" "}
+                    {entry.koi_srad} <span> Solar Radii </span>|{" "}
+                    <span className="dataName">Stellar mass: </span>{" "}
+                    {entry.koi_smass} <span> Solar Masses </span>|{" "}
+                    <span className="dataName">Stellar Age: </span>{" "}
+                    {entry.koi_sage} <span> Gigayears </span>
                   </li>
                 ))}
               </ul>
