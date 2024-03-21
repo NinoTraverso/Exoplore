3afa8f48800dfad4b9bf5d3fd46968d46be0861c
NinoTraverso <njt98git@proton.me>
2024-03-21 09:53:01 +0000

    Added link to retrieve keywords for api data

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import ReactDOM from "react-dom/client";
 import App from ".//components/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+/* API Usage and keywords: https://exoplanetarchive.ipac.caltech.edu/docs/API_kepcandidate_columns.html */
